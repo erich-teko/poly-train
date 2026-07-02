5d337faddb78074b5c0f322f70264557eeb43752
Erich-Teko <erich.imfeld@edu.teko.ch>
2026-07-02 12:48:47 +0200

    Cleanup server from sample code

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 import "dotenv/config";
 
 const app = express();
@@ -12,14 +12,6 @@ mongoose
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("Error connecting to MongoDB:", err));
 
-const userSchema = new Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-});
-
-const user = mongoose.model("user", userSchema);
-
 app.get("/", (req, res) => {
     const serverInfo = {
         name: "Poly-Train Server",
@@ -29,25 +21,6 @@ app.get("/", (req, res) => {
     };
 
     res.send(serverInfo);
-});
-
-app.get("/users", async (req, res) => {
-    try {
-        const users = await user.find();
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-app.post("/users", async (req, res) => {
-    const newUser = new user(req.body);
-    try {
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
 });
 
 app.listen(port, () => {
