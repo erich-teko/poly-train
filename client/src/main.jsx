db8d1dc312d6e0d66f2c24372795e9bd0cb5ff52
Erich-Teko <erich.imfeld@edu.teko.ch>
2026-08-18 22:23:53 +0200

    Change from TrypeScript to JavaScript

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,15 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
-import App from "./App.tsx"
+import App from "./App.jsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
   </StrictMode>
 )
+
+
