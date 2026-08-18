3f50b41728dda66bbc40b1b905c9321174e9bfaf
Erich-Teko <erich.imfeld@edu.teko.ch>
2026-08-18 22:43:30 +0200

    Add two pages with example date
    Dashboard
    SignIn

---
+++ client/pages/Dashboard.jsx
@@ -0,0 +1,17 @@
+function Dashboard() {
+  return (
+    <div className="flex min-h-svh p-6">
+      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+        <div>
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <p>Welcome to the Poly-Train dashboard!</p>
+        </div>
+        <div className="font-mono text-xs text-muted-foreground">
+          (Press <kbd>d</kbd> to toggle dark mode)
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ client/pages/SignIn.jsx
@@ -0,0 +1,10 @@
+function SignIn() {
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      {/* Add your sign-in form or components here */}
+    </div>
+  )
+}
+
+export default SignIn
