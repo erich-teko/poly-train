43527a1525cd09651e2df4461c7810fd2d473a0a
Erich-Teko <erich.imfeld@edu.teko.ch>
2026-08-18 22:09:19 +0200

    Set server port and add combined start of front- and backend together

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,9 +3,16 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+const PORT = Number(process.env.PORT || 3001);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': `http://localhost:${PORT}`,
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
