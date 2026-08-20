d68360a4f513c9515e139a6bcc104d90c44e9a09
Erich-Teko <erich.imfeld@edu.teko.ch>
2026-08-20 12:33:11 +0200

    Fix error in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,21 @@
-import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 import { defineConfig } from "vite"
 
-const PORT = Number(process.env.PORT || 3001);
+const PORT = Number(process.env.PORT || 3001)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': `http://localhost:${PORT}`,
+      "/api": `http://localhost:${PORT}`,
     },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
 })
