266c8e62b533314449faa244f21987eadc854c57
Erich-Teko <erich.imfeld@edu.teko.ch>
2026-08-25 17:29:55 +0200

    Auto format header file

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,16 @@
 import UserProfile from "./UserProfile"
 
-function Header({projectName}) {
-    return (
-        <header>
-        <div className="flex items-center justify-between bg-gray-800 p-4 text-white">
-            <h1>{projectName}</h1>
-            <UserProfile />
+function Header({ projectName }) {
+  return (
+    <header>
+      <div className="flex items-center justify-between bg-gray-800 p-4 text-white">
+        <div>
+          <h1>{projectName}</h1>
         </div>
-        </header>
-    )
-
+        <UserProfile />
+      </div>
+    </header>
+  )
 }
 
 export default Header
-
-
